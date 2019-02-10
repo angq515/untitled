eec24be416e7af210809ba9f568be80958cdf9af
angq515 <571471156@qq.com>
2019-02-10 18:12:35 +1100

    Initial commit

---
+++ 3.Python内置类型 数字.py
@@ -0,0 +1,11 @@
+import pymongo
+put('   hello world')
+put(    'hello world')
+    putput('hello wohello world')
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.5
+#
+a = -100
+if a >= 0:
+    print(a)
+else:
+    print(a)
